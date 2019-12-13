0e4583bf4adc4b32420b44923aeddd99402aa9c5
Anthony Muli <mulianthony561@gmail.com>
2019-12-13 16:28:12 +0300

    modify the functions in the js/scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// function getSize() {
+//   var size = document.getElementById("pizzaSize").value;
+//   return parseInt(size)
+// }
+
+// function getSize() {
+//   var size = document.getElementById("pizzaSize").value;
+//   return parseInt(size)
+// }
+
+// function getSize() {
+//   var size = document.getElementById("pizzaSize").value;
+//   return parseInt(size)
+// }
+
+// function getSize() {
+//   var size = document.getElementById("pizzaSize").value;
+//   return parseInt(size)
+// }
+
+// function totalAmount() {
+//   var totals = (getSize() + (getCrust() + getTopping()) * getQuantity());
+//   alert("You have ordered" + getQuantity("") + " pizza," + " " + " amounting to Ksh + (totals) + " + " Thank you
+//     for your order and feel free to reach us any time);
+//   prompt("Enter your location")
+//   alert("You order is being processed and will be delivered in a fraction of a second.Delivery fee is Ksh.200/= only");
+// }
